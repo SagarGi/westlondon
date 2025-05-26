a420d7e398067382320cc77be9f27803c9f4635f
sagar <sagargurung1001@gmail.com>
2025-05-26 16:25:33 +0100

    Question 1 Done
    Signed-off-by: sagar <sagargurung1001@gmail.com>

---
+++ dataanalytics-assessment3/q1Answer.py
@@ -0,0 +1,16 @@
+from pyspark.sql import SparkSession
+
+# Code for Answer to question 1
+# Create a Spark session so that data can be loaded with PySpark
+spark = SparkSession.builder \
+    .appName("Load the dataset into Pyspark DataFrame") \
+    .getOrCreate()
+pathToDataset = "customer_purchases_latest.csv"
+# Load the dataset into a PySpark DataFrame
+dataFrame = spark.read.csv(pathToDataset, header=True, inferSchema=True)
+# Show the first few rows of the DataFrame to verify the data has been loaded correctly
+dataFrame.show()
+# show the schema of the DataFrame
+dataFrame.printSchema()
+# show the summary statistics of the DataFrame
+dataFrame.describe().show()
